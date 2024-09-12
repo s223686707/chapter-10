dea6f23b25640578dbbe785182ea16cbdf0c3fd5
s223686707 <s223686707@deakin.edu.au>
2024-09-12 18:27:33 +1000

    updated dockerfiles

--- scripts/cd/infrastructure.sh
+++ scripts/cd/infrastructure.sh
@@ -6,5 +6,5 @@
 #   ./scripts/cd/infrastructure.sh
 #
 
-kubectl apply -f rabbit.yaml
-kubectl apply -f mongodb.yaml
+kubectl apply -f ./scripts/cd/rabbit.yaml
+kubectl apply -f ./scripts/cd/mongodb.yaml
